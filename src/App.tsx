8ff7eacd48e7a8ee1b06a3f0e5de3399986dfdd8
a.agzamov <a.agzamov@itcodegroup.ru>
2023-05-07 17:31:39 +0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import './App.scss'
+
+// @ts-ignore
+const tg = window.Telegram.WebApp
 
 const App = () => {
+	useEffect(() => {
+		tg.ready()
+	}, [])
+
 	const asd = 4
+
+	const onClose = () => tg.close()
+
 	return (
 		<div className="App">
 			<div>{asd}</div>
+			<button onClick={onClose}>Закрыть</button>
 		</div>
 	)
 }
