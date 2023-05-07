4b1406c5fcc5c5094565a5853468df8dbe3ac54f
a.agzamov <a.agzamov@itcodegroup.ru>
2023-05-07 17:57:58 +0500

    test

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
-import './assets/index.scss'
+
+// import './assets/index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
