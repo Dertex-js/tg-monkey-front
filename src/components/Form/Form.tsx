97427df3ba691d9b3af49fa7e807352424b6e82f
a.agzamov <a.agzamov@itcodegroup.ru>
2023-05-08 16:28:04 +0500

    test

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,69 +1,11 @@
-import { useTelegram } from 'hooks'
-import React, { useCallback, useEffect, useState } from 'react'
-
-import { CustomInput, CustomSelect } from 'UI'
-import { CustomOptionValue, OptionItem } from 'UI/Select/types'
+import React from 'react'
 
 import cl from './style.module.scss'
 
-const Form = () => {
-	const options: OptionItem[] = [
-		{ value: 'legal', text: 'Юр. Лицо' },
-		{ value: 'phys', text: 'Физ. Лицо' }
-	]
-
-	const [city, setCity] = useState('')
-	const [street, setStreet] = useState('')
-	const [subject, setSubject] = useState<CustomOptionValue>('')
-	const { tg } = useTelegram()
-
-	useEffect(() => {
-		tg.MainButton.setParams({
-			text: 'Отправить данные'
-		})
-	}, [])
-
-	useEffect(() => {
-		if (!city || !street || !subject) {
-			tg.MainButton.hide()
-		} else {
-			tg.MainButton.show()
-		}
-	}, [city, street])
-
-	const onChangeCity = useCallback((e: { target: { value: string } }) => {
-		setCity(e.target.value.trim())
-	}, [])
-	const onChangeStreet = useCallback((e: { target: { value: string } }) => {
-		setStreet(e.target.value.trim())
-	}, [])
-	const onChangeSubject = useCallback((value: CustomOptionValue) => {
-		setSubject(value)
-	}, [])
-
-	return (
-		<div className={cl.form}>
-			<div>asdasds</div>
-			<CustomInput
-				type="text"
-				placeholder="Город"
-				onChange={onChangeCity}
-				value={city}
-			/>
-			<CustomInput
-				type="text"
-				placeholder="Улица"
-				onChange={onChangeStreet}
-				value={street}
-			/>
-			<CustomSelect
-				optionItemsList={options}
-				value={subject}
-				placeholder="Выберите"
-				onChange={onChangeSubject}
-			/>
-		</div>
-	)
-}
+const Form = () => (
+	<div className={cl.form}>
+		<div>asdasds</div>
+	</div>
+)
 
 export default Form
