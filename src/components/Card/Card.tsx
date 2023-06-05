c1883956da80004909415b9bc1f9076400724076
a.agzamov <a.agzamov@itcodegroup.ru>
2023-06-05 11:52:22 +0500

    main routes + tailwind

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -9,16 +9,16 @@ interface CardProps {
 const Card: FC<CardProps> = ({ title, route }) => {
 	const navigate = useNavigate()
 
-	const handleClick = useCallback(() => {
+	const handleRoutePush = useCallback(() => {
 		navigate(route)
 	}, [])
 
 	return (
 		<div
 			className="flex h-full w-full items-center justify-center rounded-lg border bg-gray-500"
-			onClick={handleClick}
+			onClick={handleRoutePush}
 		>
-			<span className="text-white">{title}</span>
+			<span className="text-lg text-white">{title}</span>
 		</div>
 	)
 }
